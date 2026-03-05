67c7b3ccec338bfd4194caeb3235321d082595d1
Heloa Mattei <heloa.mattei09@gmail.com>
2026-03-05 19:03:45 -0300

    adiciona contagem regressiva e atualiza o layout da página

---
+++ tempo.java
@@ -0,0 +1,26 @@
+
+<script>
+var dataAlvo = new Date("Jun 10, 2026 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var agora = new Date().getTime();
+  var distancia = dataAlvo - agora;
+
+  var dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+  var horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+  var segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+  dias + "d " + horas + "h "
+  + minutos + "m " + segundos + "s ";
+
+  if (distancia < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML =
+    "Feliz 1 ano de namoro ❤️";
+  }
+
+}, 1000);
+</script>
